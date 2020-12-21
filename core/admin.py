a754cdca0b56b0cbd4b074646455e2f9e8d5e4bb
EU\dlubajac <jacekdlubak07@gmail.com>
2020-12-21 21:51:05 +0100

    Register user into admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from core.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    search_fields = ('email',)
+    ordering = ('id',)
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('email', 'name')
+    fieldsets = (
+        (None, {"fields": ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {'fields': ('is_active', 'is_staff', 'is_superuser',)}
+        ),
+        (_('Important dates'), {'fields': ('last_login',)})
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
